c7a85d4d12d2574680ac62513469052b31c798a0
Suriyanath S <ssuriyanath@gmail.com>
2020-08-11 09:04:29 +0000

    change code

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Test My Code Test againTest" );
+        System.out.println( "something new test" );
     }
 
 	public int calculateSomething() {
